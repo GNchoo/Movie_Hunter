19b70e45a523b65287430450fdc4a9120d97eb25
ririariris <ehrkdrb1@gmail.com>
2023-05-03 17:30:49 +0900

    처음 업로드

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,100 @@
+import React, { useRef, useEffect, useState } from "react";
+
+import { Link, useLocation } from "react-router-dom";
+
+import "./header.scss";
+
+import logo from "../../assets/movielogo.png";
+
+const headerNav = [
+  {
+    display: "Home",
+    path: "/",
+  },
+  {
+    display: "검색 & 추천",
+    path: "/ChuChon",
+  },
+  {
+    display: "영화",
+    path: "/movie",
+  },
+  {
+    display: "TV 시리즈",
+    path: "/tv",
+  },
+  {
+    display: "게시판",
+    path: "/Board",
+  },
+];
+
+const Header = () => {
+  const { pathname } = useLocation();
+  const headerRef = useRef(null);
+  const location = useLocation();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const active = headerNav.findIndex((e) => e.path === pathname);
+
+  useEffect(() => {
+    const id = localStorage.getItem("id");
+    setIsLoggedIn(id != null);
+
+    const shrinkHeader = () => {
+      if (
+        document.body.scrollTop > 100 ||
+        document.documentElement.scrollTop > 100
+      ) {
+        headerRef.current.classList.add("shrink");
+      } else {
+        headerRef.current.classList.remove("shrink");
+      }
+    };
+    window.addEventListener("scroll", shrinkHeader);
+    return () => {
+      window.removeEventListener("scroll", shrinkHeader);
+    };
+  });
+
+  const handleLogout = () => {
+    localStorage.clear();
+    alert("로그아웃 완료");
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <div ref={headerRef} className="header">
+      <div className="header__wrap container">
+        <div className="logo">
+          <img src={logo} alt="" />
+          <Link to="/">Movie Hunter</Link>
+        </div>
+        <ul className="header__nav">
+          {headerNav.map((e, i) => (
+            <li key={i} className={`${i === active ? "active" : ""}`}>
+              <Link to={e.path}>{e.display}</Link>
+            </li>
+          ))}
+        </ul>
+        <div>
+          {location.pathname === "/login" ? (
+            <Link to={"/sign"}>
+              <button>회원가입</button>
+            </Link>
+          ) : isLoggedIn ? (
+            <Link to={"/"}>
+              <button onClick={handleLogout}>로그아웃</button>
+            </Link>
+          ) : (
+            <Link to={"/login"}>
+              <button>로그인</button>
+            </Link>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/pages/detail/Login.jsx
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from "react";
+import Button from "../../components/button/Button";
+import Input from "../../components/input/Input";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { ServerApi } from "../../api/ServerApi";
+
+import "./Login.scss";
+
+import bg from "../../assets/body-bg.jpg";
+import bg2 from "../../assets/login-bg.jpg";
+const Login = (props) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (localStorage.getItem("id")) {
+      navigate("/home");
+    }
+  }, [navigate]);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post(`${ServerApi}/login/`, {
+        username,
+        password,
+      });
+      console.log(response.data);
+      alert("로그인 성공");
+      localStorage.setItem("id", response.data.username);
+      navigate("/");
+    } catch (error) {
+      alert("아이디나 비밀번호를 다시 확인해주세요.");
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <div className="page-header" style={{ backgroundImage: `url(${bg})` }}>
+        <h2>로그인</h2>
+      </div>
+      <div
+        className="login-container"
+        style={{ backgroundImage: `url(${bg2})` }}
+      >
+        <form onSubmit={handleSubmit} style={{ textAlign: "center" }}>
+          <div className="id">
+            <Input
+              type="text"
+              placeholder="아이디 입력"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div className="password">
+            <Input
+              type="password"
+              placeholder="비밀번호 입력"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <br />
+          <Button className="small" type="submit">
+            로그인
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
