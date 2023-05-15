b2eb38a081cc1115c9a2fe1a782f40eb00a74baf
spaceJo96 <joalstn11@naver.com>
2023-05-15 16:47:14 +0900

    영화 한줄평 CRUD 기능 구현

--- server/routes/Movie/movie.js
+++ server/routes/Movie/movie.js
@@ -6,11 +6,12 @@ const mongodb = require("../../db/db");
 mongodb.connect();
 
 router.get(`/:id`, (req, res) => {
+  const id = parseInt(req.params.id);
   console.log("영화 상세페이지");
 });
 
 // 영화 한줄평 목록
-router.get("/:id/comment", (req, res) => {
+router.get("/:id", (req, res) => {
   const id = req.params.id;
   const db = mongodb.getDB();
 
@@ -26,28 +27,66 @@ router.get("/:id/comment", (req, res) => {
 });
 
 // 영화 한줄평 등록
-router.post(`/:id/comment/add`, (req, res) => {
+router.post(`/:id/add`, (req, res) => {
   const id = req.params.id; // 클라이언트 요청에서 id 값을 추출
 
   const db = mongodb.getDB();
+  db.collection("counter").findOne({ name: "comments" }, (err, result) => {
+    var totalPost = result.totalPost;
 
-  db.collection("comment").insertOne({
-    movieId: id,
-    text: req.body.text,
-    writer: req.body.user,
-    sex: "미정",
-    birth: parseInt("2023" - "미정"),
-    start: parseInt("미정"), // 별점기능
-    date: new Date().toLocaleString("ko-KR", {
-      timeZone: "Asia/Seoul",
-    }),
+    db.collection("comment").insertOne({
+      _id: totalPost + 1,
+      movieId: id,
+      text: req.body.text,
+      writer: req.body.user,
+      // sex: "미정",
+      // birth: parseInt("2023" - "미정"),
+      // star: parseInt("미정"), // 별점기능
+      date: new Date().toLocaleString("ko-KR", {
+        timeZone: "Asia/Seoul",
+      }),
+    });
+    (err, result) => {
+      if (err) {
+        return console.log(err);
+      }
+      res.redirect("/movie/:id");
+    };
   });
-  (err, result) => {
+});
+
+router.delete("/:id", (req, res) => {
+  const _id = parseInt(req.body._id);
+  const db = mongodb.getDB();
+
+  db.collection("comment").deleteOne({ _id: _id }, (err, result) => {
     if (err) {
       return console.log(err);
     }
-    res.redirect("/movie/:id/comment");
-  };
+    console.log(`삭제되었습니다.`);
+    res.sendStatus(204); // 클라이언트에게 성공적으로 처리되었음을 알립니다.
+  });
+});
+
+router.put("/:id", (req, res) => {
+  const _id = parseInt(req.body._id);
+  const updatedCommentdData = req.body;
+  const db = mongodb.getDB();
+
+  db.collection("comment").updateOne(
+    { _id: _id },
+    {
+      $set: {
+        text: updatedCommentdData.text,
+        star: updatedCommentdData.start,
+      },
+    },
+    (err, result) => {
+      if (err) return console.log(err);
+      console.log("글 수정 완료");
+      res.send(result);
+    }
+  );
 });
 
 module.exports = router;
